20be4f06803db16b9946a646449c6d974cc4c7d1
alex liu <1280310160@qq.com>
2018-01-10 18:19:01 +0800

    重命名文件

--- PhpFileRename.php
+++ PhpFileRename.php
@@ -1,10 +1,10 @@
 <?php
-//$arr = glob("C:/Users/liux/Desktop/old/*.jpg");
-//foreach ($arr as $k => $v) {
-//    $filename1 = 'C:/Users/liux/Desktop/card/20180110_' . ($k + 1) . '.jpg';
-//    echo $v . "----->" . $filename1 . "<br>";
-//    rename($v, $filename1);
-//}
+$arr = glob("C:/Users/liux/Desktop/old/*.jpg");
+foreach ($arr as $k => $v) {
+    $filename1 = 'C:/Users/liux/Desktop/card/20180110_' . ($k + 1) . '.jpg';
+    echo $v . "----->" . $filename1 . "<br>";
+    rename($v, $filename1);
+}
 
 
 # -----------------------------------------
