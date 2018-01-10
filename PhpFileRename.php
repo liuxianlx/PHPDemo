df0e480d1a8fa1994f6d846b5e5530574da0ec3f
alex liu <1280310160@qq.com>
2018-01-10 18:07:24 +0800

    重命名文件

--- PhpFileRename.php
+++ PhpFileRename.php
@@ -1,15 +1,15 @@
 <?php
-$arr = glob("C:/Users/liux/Desktop/old/*.jpg");
-foreach ($arr as $k => $v) {
-    $filename1 = 'C:/Users/liux/Desktop/card/20180110_' . ($k + 1) . '.jpg';
-    echo $v . "----->" . $filename1 . "<br>";
-    rename($v, $filename1);
-}
+//$arr = glob("C:/Users/liux/Desktop/old/*.jpg");
+//foreach ($arr as $k => $v) {
+//    $filename1 = 'C:/Users/liux/Desktop/card/20180110_' . ($k + 1) . '.jpg';
+//    echo $v . "----->" . $filename1 . "<br>";
+//    rename($v, $filename1);
+//}
 
 
 # -----------------------------------------
 
-$dirname = 'c:/wamp/www/zzz';
+$dirname = 'c:/Users/liux/Desktop/card';
 $handle = opendir($dirname);
 while (($fn = readdir($handle)) !== false) {
     if ($fn != '.' && $fn != '..') {
@@ -19,7 +19,7 @@ while (($fn = readdir($handle)) !== false) {
             echo "Is dir: $path[basename]";
         } else {
             var_dump($path['basename']);
-            // $newname = ???// 去掉A后的新名字
+            // $newname = ???// 新名字
             // rename($curDir,$newname);
         }
     }
